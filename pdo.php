48a0a4c00c9755fcaa5ae0b515c248a283bdfc32
nanodotbot <nanodotbot@gmail.com>
2023-10-08 17:13:59 +0200

    git restore --staged pdo.php

--- pdo.php
+++ pdo.php
@@ -1,4 +1,4 @@
 <?php 
-$pdo = new PDO('sqlite:./chat.db');
-// $pdo = new PDO('sqlite:./host=localhost;dbname=ch362613_chat','ch362613_root','kaviar&Koks=0');
+// $pdo = new PDO('sqlite:./chat.db');
+$pdo = new PDO('sqlite:./host=localhost;dbname=ch362613_chat','ch362613_root','kaviar&Koks=0');
 ?>
